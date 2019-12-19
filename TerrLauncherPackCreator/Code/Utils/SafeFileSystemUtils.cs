cb8a47a172f03007f2534ccce3c2b8e950062a0d
And42 <andrewskobcov@yandex.ru>
2019-12-19 14:41:02 +0300

    Added safe filesystem utils

---
+++ TerrLauncherPackCreator/Code/Utils/SafeFileSystemUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using JetBrains.Annotations;
+
+namespace TerrLauncherPackCreator.Code.Utils
+{
+    public static class SafeFileSystemUtils
+    {
+        public static void DeleteFile([NotNull] string file)
+        {
+            if (File.Exists(file))
+                File.Delete(file);
+        }
+
+        public static void DeleteDir([NotNull] string dir)
+        {
+            if (Directory.Exists(dir))
+                Directory.Delete(dir, true);
+        }
+    }
+}
