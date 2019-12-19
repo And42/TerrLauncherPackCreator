39de77473c7c3a5df741b8859fc1c44f1d7cf454
And42 <andrewskobcov@yandex.ru>
2019-12-19 14:40:49 +0300

    Only file name visible in modified files

---
+++ TerrLauncherPackCreator/Code/Converters/PathToFileNameConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.IO;
+using MVVM_Tools.Code.Classes;
+
+namespace TerrLauncherPackCreator.Code.Converters
+{
+    public class PathToFileNameConverter : ConverterBase<string, string>
+    {
+        public override string ConvertInternal(string value, object parameter, CultureInfo culture)
+        {
+            return Path.GetFileNameWithoutExtension(value);
+        }
+    }
+}
