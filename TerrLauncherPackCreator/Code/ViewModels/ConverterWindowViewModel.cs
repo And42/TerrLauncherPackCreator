d79195e8b91092be1a49594d00db07386ed41ba5
And42 <andrewskobcov@yandex.ru>
2020-05-03 18:48:36 +0300

    Added backward conversion to Converter

--- TerrLauncherPackCreator/Code/ViewModels/ConverterWindowViewModel.cs
+++ TerrLauncherPackCreator/Code/ViewModels/ConverterWindowViewModel.cs
@@ -9,6 +9,7 @@ using MVVM_Tools.Code.Commands;
 using TerrLauncherPackCreator.Code.Enums;
 using TerrLauncherPackCreator.Code.Implementations;
 using TerrLauncherPackCreator.Code.Interfaces;
+using TerrLauncherPackCreator.Code.Json;
 using TerrLauncherPackCreator.Code.Utils;
 
 namespace TerrLauncherPackCreator.Code.ViewModels
@@ -21,7 +22,10 @@ namespace TerrLauncherPackCreator.Code.ViewModels
             set => SetProperty(ref _currentFileType, value);
         }
 
-        public string CurrentFilesExtension => PackUtils.PacksInfo.First(it => it.fileType == CurrentFileType).initialFilesExt;
+        [NotNull]
+        public string SourceFilesExtension => PackUtils.GetInitialFilesExt(CurrentFileType);
+        [NotNull]
+        public string ConvertedFilesExtension => PackUtils.GetConvertedFilesExt(CurrentFileType);
 
         public FileType[] FileTypes { get; } = PackUtils.PacksInfo.Select(it => it.fileType).ToArray();
 
@@ -30,25 +34,28 @@ namespace TerrLauncherPackCreator.Code.ViewModels
         private FileType _currentFileType = FileType.Texture;
         
         [NotNull]
-        public IActionCommand<string[]> DropFilesCommand { get; }
+        public IActionCommand<string[]> DropSourceFilesCommand { get; }
+        [NotNull]
+        public IActionCommand<string[]> DropConvertedFiledCommand { get; }
 
         public ConverterWindowViewModel()
         {
-            DropFilesCommand = new ActionCommand<string[]>(DropFilesCommand_Execute, DropFilesCommand_CanExecute);
+            DropSourceFilesCommand = new ActionCommand<string[]>(DropSourceFilesCommand_Execute, DropSourceFilesCommand_CanExecute);
+            DropConvertedFiledCommand = new ActionCommand<string[]>(DropConvertedFilesCommand_Execute, DropConvertedFilesCommand_CanExecute);
             
             PropertyChanged += OnPropertyChanged;
         }
 
-        private bool DropFilesCommand_CanExecute(string[] files)
+        private bool DropSourceFilesCommand_CanExecute(string[] files)
         {
             if (Working)
                 return false;
 
-            string currentExt = CurrentFilesExtension;
+            string currentExt = SourceFilesExtension;
             return files.All(it => Path.GetExtension(it) == currentExt);
         }
 
-        private async void DropFilesCommand_Execute(string[] files)
+        private async void DropSourceFilesCommand_Execute(string[] files)
         {
             using (LaunchWork())
             {
@@ -73,15 +80,89 @@ namespace TerrLauncherPackCreator.Code.ViewModels
             }
         }
 
+        private bool DropConvertedFilesCommand_CanExecute(string[] files)
+        {
+            if (Working)
+                return false;
+
+            string currentExt = ConvertedFilesExtension;
+            return files.All(it => Path.GetExtension(it) == currentExt);
+        }
+
+        private async void DropConvertedFilesCommand_Execute(string[] files)
+        {
+            using (LaunchWork())
+            {
+                try
+                {
+                    foreach (string file in files)
+                    {
+                        string resultsDir = Path.Combine(Path.GetDirectoryName(file), "source_files");
+                        IOUtils.EnsureDirExists(resultsDir);
+
+                        string config = Path.ChangeExtension(file, PackUtils.PackFileConfigExtension);
+                        if (!File.Exists(config))
+                            config = null;
+        
+                        var (convertedFile, fileInfo) = await _fileConverter.ConvertToSource(CurrentFileType, file, config);
+                        string resultFileExt = PackUtils.GetInitialFilesExt(CurrentFileType);
+
+                        string resultFileName = Path.GetFileNameWithoutExtension(file);
+                        if (fileInfo != null)
+                        {
+                            switch (CurrentFileType)
+                            {
+                                case FileType.Texture:
+                                    var textureInfo = (TextureFileInfo) fileInfo;
+                                    resultFileName = textureInfo.EntryName;
+                                    break;
+                                case FileType.Map:
+                                    var mapInfo = (MapFileInfo) fileInfo;
+                                    resultFileName = mapInfo.ResultFileName;
+                                    break;
+                                case FileType.Character:
+                                    break;
+                                case FileType.Gui:
+                                    var guiInfo = (GuiFileInfo) fileInfo;
+                                    resultFileName = guiInfo.EntryName;
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                        }
+                        
+                        foreach (char invalidChar in Path.GetInvalidFileNameChars())
+                            resultFileName = resultFileName.Replace(invalidChar, '_');
+
+                        string resultFile;
+                        int index = 1;
+                        do
+                        {
+                            resultFile = Path.Combine(resultsDir, resultFileName + (index == 1 ? "" : "_" + index) + resultFileExt);
+                            index++;
+                        } while (File.Exists(resultFile));
+                        
+                        File.Copy(convertedFile, resultFile, overwrite: true);
+                        File.Delete(convertedFile);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unknown exception: " + ex);
+                }
+            }
+        }
+        
         private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             switch (e.PropertyName)
             {
                 case nameof(CurrentFileType):
-                    OnPropertyChanged(nameof(CurrentFilesExtension));
+                    OnPropertyChanged(nameof(SourceFilesExtension));
+                    OnPropertyChanged(nameof(ConvertedFilesExtension));
                     break;
                 case nameof(Working):
-                    DropFilesCommand.RaiseCanExecuteChanged();
+                    DropSourceFilesCommand.RaiseCanExecuteChanged();
                     break;
             }
         }
--- TerrLauncherPackCreator/Windows/ConverterWindow.xaml.cs
+++ TerrLauncherPackCreator/Windows/ConverterWindow.xaml.cs
@@ -19,14 +19,24 @@ namespace TerrLauncherPackCreator.Windows
             set => DataContext = value;
         }
 
-        private void Files_OnDragOver(object sender, DragEventArgs e)
+        private void SourceFiles_OnDragOver(object sender, DragEventArgs e)
         {
-            DragDropUtils.HandleDrag(e, ViewModel.DropFilesCommand.CanExecute, DragDropEffects.Copy);
+            DragDropUtils.HandleDrag(e, ViewModel.DropSourceFilesCommand.CanExecute, DragDropEffects.Copy);
         }
 
-        private void Files_OnDrop(object sender, DragEventArgs e)
+        private void SourceFiles_OnDrop(object sender, DragEventArgs e)
         {
-            DragDropUtils.HandleDrop(e, ViewModel.DropFilesCommand.CanExecute, ViewModel.DropFilesCommand.Execute);
+            DragDropUtils.HandleDrop(e, ViewModel.DropSourceFilesCommand.CanExecute, ViewModel.DropSourceFilesCommand.Execute);
+        }
+        
+        private void ConvertedFiles_OnDragOver(object sender, DragEventArgs e)
+        {
+            DragDropUtils.HandleDrag(e, ViewModel.DropConvertedFiledCommand.CanExecute, DragDropEffects.Copy);
+        }
+
+        private void ConvertedFiles_OnDrop(object sender, DragEventArgs e)
+        {
+            DragDropUtils.HandleDrop(e, ViewModel.DropConvertedFiledCommand.CanExecute, ViewModel.DropConvertedFiledCommand.Execute);
         }
     }
 }
