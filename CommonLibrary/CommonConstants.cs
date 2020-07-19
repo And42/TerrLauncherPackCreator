672716a29d372cd8d8b2d4692968dea96d8145d3
And42 <andrewskobcov@yandex.ru>
2020-07-19 14:53:53 +0300

    Preview branch

--- CommonLibrary/CommonConstants.cs
+++ CommonLibrary/CommonConstants.cs
@@ -2,16 +2,18 @@
 {
     public static class CommonConstants
     {
-        public const bool IsPreview = false;
+        public const bool IsPreview = true;
 
         public const string VersionFileUrl = IsPreview
             // ReSharper disable once UnreachableCode
             ? "https://terrlauncher.ams3.cdn.digitaloceanspaces.com/pc_pack_creator/prevew_version.txt"
+            // ReSharper disable once UnreachableCode
             : "https://terrlauncher.ams3.cdn.digitaloceanspaces.com/pc_pack_creator/master_version.txt";
         
         public const string LatestVersionZipUrl = IsPreview
             // ReSharper disable once UnreachableCode
             ? "https://terrlauncher.ams3.cdn.digitaloceanspaces.com/pc_pack_creator/prevew.zip"
+            // ReSharper disable once UnreachableCode
             : "https://terrlauncher.ams3.cdn.digitaloceanspaces.com/pc_pack_creator/master.zip";
     }
 }
