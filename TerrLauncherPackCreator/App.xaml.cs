d2664e1583e4413acb3df71b92cf2e6ce852310b
And42 <andrewskobcov@yandex.ru>
2021-02-10 13:04:32 +0300

    Updated to Net5.0

--- TerrLauncherPackCreator/App.xaml.cs
+++ TerrLauncherPackCreator/App.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
