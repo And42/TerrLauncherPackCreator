f201cb7e5321b791926c475054370ca4056abd98
And42 <andrewskobcov@yandex.ru>
2021-03-04 13:20:13 +0300

    Fixed app path for single exe

--- TerrLauncherPackCreator/Code/Utils/Paths.cs
+++ TerrLauncherPackCreator/Code/Utils/Paths.cs
@@ -1,17 +1,14 @@
-﻿using System.IO;
-using System.Reflection;
+﻿using System;
+using System.IO;
 
 namespace TerrLauncherPackCreator.Code.Utils
 {
     public static class Paths
     {
 #if DEBUG
-        public static readonly string ExeDir = Path.GetFullPath(Path.Combine(
-            Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
-            "..", "Release"
-        ));
+        public static readonly string ExeDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "Release"));
 #else
-        public static readonly string ExeDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        public static readonly string ExeDir = AppContext.BaseDirectory;
 #endif
 
         public static readonly string DataDir = Path.Combine(ExeDir, "data");
