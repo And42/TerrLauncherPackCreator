41934c6d0f729bc17358534dafc8b4e4a58ed3b9
And42 <andrewskobcov@yandex.ru>
2023-04-24 20:39:25 +0700

    Added IsNullOrEmpty extension method

---
+++ CommonLibrary/CommonUtils/StringUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CommonLibrary.CommonUtils;
+
+public static class StringUtils
+{
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+}
