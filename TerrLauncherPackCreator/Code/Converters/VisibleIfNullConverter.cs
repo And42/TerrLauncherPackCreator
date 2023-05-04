cc0c982cb48ccb920b6bfee56eeb1d8ad336cad8
And42 <andrewskobcov@yandex.ru>
2023-05-04 22:21:38 +0700

    Fixed icon drop place visibility

--- TerrLauncherPackCreator/Code/Converters/VisibleIfNullConverter.cs
+++ TerrLauncherPackCreator/Code/Converters/VisibleIfNullConverter.cs
@@ -4,9 +4,9 @@ using MVVM_Tools.Code.Classes;
 
 namespace TerrLauncherPackCreator.Code.Converters;
 
-internal class VisibleIfNullConverter : ConverterBase<object, Visibility>
+internal class VisibleIfNullConverter : ConverterBase<object?, Visibility>
 {
-    public override Visibility ConvertInternal(object value, object parameter, CultureInfo culture)
+    public override Visibility ConvertInternal(object? value, object parameter, CultureInfo culture)
     {
         return value == null ? Visibility.Visible : Visibility.Collapsed;
     }
---
+++ TerrLauncherPackCreator/Code/Converters/VisibleIfNullOrEmptyConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Windows;
+using CommonLibrary.CommonUtils;
+using MVVM_Tools.Code.Classes;
+
+namespace TerrLauncherPackCreator.Code.Converters;
+
+internal class VisibleIfNullOrEmptyConverter : ConverterBase<string?, Visibility>
+{
+    public override Visibility ConvertInternal(string? value, object parameter, CultureInfo culture)
+    {
+        return value.IsNullOrEmpty() ? Visibility.Visible : Visibility.Collapsed;
+    }
+}
